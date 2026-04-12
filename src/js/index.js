fe4e3a6a6f59f5af32a28c5c15e6238310056136
Ciaomicio <tablet722imediacom@gmail.com>
2026-04-12 22:58:52 +0200

    changes index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,19 @@
 (function(context){
-    var wallet;
+    let wallet;
     context.addOperation = function (op){
-        wallet.addOperation(op);
+        try{
+            wallet.addOperation(op);
+        } catch(e){
+            console.error(e);
+        }
     }
 
     context.removeOperation = function (id){
-        wallet.removeOperation(id);
+        try{
+            wallet.removeOperation(id);
+        } catch(e){
+            console.error(e);
+        }
     }
 
     context.findOperation = function (val){
