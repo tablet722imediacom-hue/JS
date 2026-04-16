311842ce89ae578b3cc919e1959449f1f055822c
Ciaomicio <tablet722imediacom@gmail.com>
2026-04-16 23:05:06 +0200

    correzione pt2

--- gulpfile.js/jsTasks.js
+++ gulpfile.js/jsTasks.js
@@ -3,14 +3,14 @@ const paths = require("./paths");
 
 const copyJs = function(){
     const jsIndex = "./src/js/index.js";
-    const utilsIndex = paths.getJsEntryPath("/utils.js");
-    const modelsIndex = paths.getJsEntryPath("/models/Wallet.js");
+    const utilsIndex = paths.getJsSrcPath("/utils.js");
+    const modelsIndex = paths.getJsSrcPath("/models/Wallet.js");
     return gulp.src([jsIndex, utilsIndex, modelsIndex], {base: paths.getSrcFolder()})
         .pipe(gulp.dest(paths.getDistFolder));
 };
 
 const watchJS = function (cb){
-    gulp.watch("/**/*", copyJs)
+    gulp.watch(paths.getJsSrcPath("/**/*"), copyJs);
     cb();
 };
 
